e314d7f053d989985b61cfaf03a77bb676cffff1
Prakhar Agarwal <prakhar.php@gmail.com>
2025-07-31 20:53:38 +0530

    completed minikube local setup

--- app/database.py
+++ app/database.py
@@ -3,8 +3,13 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+DB_HOST = os.getenv("DATABASE_HOST", "localhost")
+DB_PORT = os.getenv("DATABASE_PORT", "5432")
+DB_NAME = os.getenv("DATABASE_NAME", "mydb")
+DB_USER = os.getenv("DATABASE_USER", "myuser")
+DB_PASSWORD = os.getenv("DATABASE_PASSWORD", "mypassword")
 # SQLALCHEMY_DATABASE_URL = "sqlite:////data/items.db"  # file-based DB
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
     engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
